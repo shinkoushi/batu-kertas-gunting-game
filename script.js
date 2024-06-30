ea3ed62a723f6f1bdc2aaf53c9f05c8bd000c08f
shin <121587677+shinkoushi@users.noreply.github.com>
2024-06-30 09:05:40 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Get  to DOM elements
 const gameContainer = document.querySelector(".container"),
   userResult = document.querySelector(".user_result img"),
   cpuResult = document.querySelector(".cpu_result img"),
@@ -9,8 +8,8 @@ const gameContainer = document.querySelector(".container"),
   let userPoints = 0
   let cpuPoints = 0
 
-  const audioElement = new Audio('audio/peluit.mp3'); 
-// Loop through each option image element
+const audioElement = new Audio('audio/peluit.mp3'); 
+
 optionImages.forEach((image, index) => {
   image.addEventListener("click", (e) => {
     image.classList.add("active");
@@ -18,46 +17,33 @@ optionImages.forEach((image, index) => {
     userResult.src = cpuResult.src = "images/batu.png";
     result.textContent = "Tunggu...";
 
-    // Loop through each option image again
+   
     optionImages.forEach((image2, index2) => {
-      // If the current index doesn't match the clicked index
-      // Remove the "active" class from the other option images
       index !== index2 && image2.classList.remove("active");
     });
 
     gameContainer.classList.add("start");
 
-    // Set a timeout to delay the result calculation
     let time = setTimeout(() => {
       gameContainer.classList.remove("start");
 
-      // Get the source of the clicked option image
       let imageSrc = e.target.querySelector("img").src;
-      // Set the user image to the clicked option image
       userResult.src = imageSrc;
-
-      // Generate a random number between 0 and 2
+     
       let randomNumber = Math.floor(Math.random() * 3);
-      // Create an array of CPU image options
       let cpuImages = ["images/batu.png", "images/kertas.png", "images/gunting.png"];
-      // Set the CPU image to a random option from the array
       cpuResult.src = cpuImages[randomNumber];
-
-      // Assign a letter value to the CPU option (R for rock, P for paper, S for scissors)
       let cpuValue = ["R", "P", "S"][randomNumber];
-      // Assign a letter value to the clicked option (based on index)
       let userValue = ["R", "P", "S"][index];
-
-      // Create an object with all possible outcomes
       let outcomes = {
         RR: "Draw",
-        RP: "Cpu",
+        RP: "Komputer",
         RS: "Kamu",
         PP: "Draw",
         PR: "Kamu",
-        PS: "Cpu",
+        PS: "Komputer",
         SS: "Draw",
-        SR: "Cpu",
+        SR: "Komputer",
         SP: "Kamu",
       };
 
@@ -66,7 +52,7 @@ optionImages.forEach((image, index) => {
           document.querySelector(".comp__score").innerHTML = cpuPoints
        }
        
-      // Look up the outcome value based on user and CPU options
+     
       let outComeValue = outcomes[userValue + cpuValue];
       if(userValue === "P" && cpuValue === "R" || userValue === "R" && cpuValue === "S" || userValue === "S" && cpuValue === "P"){
           userPoints++
@@ -78,7 +64,7 @@ optionImages.forEach((image, index) => {
         score()
       }
       
-      // Display the result
+      
       result.textContent = userValue === cpuValue ? "Seri nih !!" : `${outComeValue} Menang!!`;
     }, 2500);
 
